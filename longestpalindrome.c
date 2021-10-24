6ce6b37e7c6e63d2685c7f069ffaac0e7a4c9a1b
ShivaPradhyumna <93043998+ShivaPradhyumna@users.noreply.github.com>
2021-10-24 15:13:22 +0530

    Create longestpalindrome.c

---
+++ longestpalindrome.c
@@ -0,0 +1,7 @@
+// palindrome function
+
+//code to find substrings and verify palindrome or not
+
+//update all palindrome substrings to an array of string
+
+//code to find longest palindrome in the array of strings(array of palindromes)
